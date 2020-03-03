8100a064a4f9a5ea1dbb881ae1ff2a7d8edc60ad
Daniel <danielandro11@gmail.com>
2020-03-03 20:07:20 +0200

    Update shit script

--- extract_paths/hashlist.lua
+++ extract_paths/hashlist.lua
@@ -1,5 +1,6 @@
 local extract = "D:/Extract/" --Change to your path
 local done = {}
+local file
 function ParseXml(file, scriptdata, trykey)
     if trykey then
         local file = ParseXml(trykey, scriptdata)
@@ -9,27 +10,24 @@ function ParseXml(file, scriptdata, trykey)
     end
     return SystemFS:exists(file) and (scriptdata and FileIO:ReadScriptData(file, "binary") or SystemFS:parse_xml(file)) or nil
 end
- 
-function LogFound(path, check_db_ext)
+
+function LogFound(path, check_db_ext, skip_check)
     if not path then return end
-    if done[path] then
-        return
-    else
-        done[path] = true
+    if not skip_check then
+        if done[path] then
+            return
+        else
+            done[path] = true
+        end
     end
     if check_db_ext then 
-        if not DB:has(check_db_ext, path) then
+        if not DB:has(check_db_ext:id(), path) then
             return
         end
     end
-    local file_write = "Paths.txt"
-    local opened_file = io.open( file_write , "a+")
-    if opened_file then
-        opened_file:write(path .. "\n")
-        opened_file:close()
-    end
+    file:write(path .. "\n")
 end
- 
+
 function SearchInUnit(file)
     local node = ParseXml(file)
     if node then
@@ -56,10 +54,10 @@ function SearchInUnit(file)
                     LogFound(remote_unit:gsub("_husk", ""), "unit")                    
                 end
             end
-        end    
+        end
     end
 end
- 
+
 function SearchInObject(file)
     local node = ParseXml(file)
     if node then
@@ -80,8 +78,7 @@ function SearchInObject(file)
         end
     end
 end
- 
- 
+
 function SearchInMaterial(file)
     local node = ParseXml(file)
     if node then
@@ -96,7 +93,7 @@ function SearchInMaterial(file)
         end
     end
 end
- 
+
 function SearchInAnimDef(file)
     local node = ParseXml(file)
     if node then
@@ -105,12 +102,12 @@ function SearchInAnimDef(file)
                 for anim_set in child:children() do
                     local anim_subset = anim_set:parameter("file")
                     LogFound(anim_subset)
-                end  
+                end
             end
         end
     end
 end
- 
+
 function SearchInAnimSubset(file)
     local node = ParseXml(file)
     if node then
@@ -214,14 +211,61 @@ function SearchInSequence(file)
     end
 end
 
-function SearchInBank(path)
-    local file = io.open(path, "rb")
+function SearchInBank(file)
+    local file = io.open(file, "rb")
     local read = file:read("*all"):reverse()
     local match = read:match("%w+_*%w*_*%w*")
     if match:len() > 2 then
         LogFound("soundbanks/"..match) --Some don't have a name at the end so they will fail.
     end
     file:close()
+end
+
+function SearchInScene(file) 
+    local node = ParseXml("scene")
+    if node then
+		for child in node:children() do
+            if child:name() == "load_scene" and child:has_parameter("materials") then
+                LogFound(child:parameter("materials"))
+			end
+        end
+    end
+end
+
+function SearchInEnvironment(file)
+	local tbl = ParseXml(file, true)
+    if tbl and tbl.data and tbl.data.others and tbl.data.others.underlay then
+        LogFound(tbl.data.others.underlay)
+	end
+	return tbl ~= nil
+end
+
+function SearchInEffect(file)
+	local node = ParseXml(file)    
+    if node then
+        for eff_child in node:children() do
+            local name = eff_child:name()
+            if name == "atom" then
+                for eff_child_x2 in eff_child:children() do
+                    local name = eff_child_x2:name()
+                    if name == "visualizerstack" then
+                        for eff_child_x3 in eff_child_x2:children() do
+                            if eff_child_x3:has_parameter("texture") then
+                                LogFound(eff_child_x3:parameter("texture"))
+                            end
+                            if eff_child_x3:has_parameter("material_config") then
+                                LogFound(eff_child_x3:parameter("material_config"))
+                            end
+                        end
+                    elseif name == "effect_spawn" then
+                        LogFound(eff_child_x2:parameter("effect"))
+                    end
+                end
+            elseif name == "use" then
+                LogFound(eff_child:parameter("name"))
+            end
+        end
+    end
 end
 
 local funcs = {
@@ -235,7 +279,10 @@ local funcs = {
     sequence_manager = SearchInSequence,
     mission = SearchInMission,
     continent = SearchInContinent,
-    world = SearchInWorld
+    world = SearchInWorld,
+    scene = SearchInScene,
+    environment = SearchInEnvironment,
+    effect = SearchInEffect
 }
 
 local logged = {}
@@ -258,10 +305,33 @@ function find_all(path)
     end
 end
 
+local did = {}
+function tweakdata_sniffer(tbl)
+    for k,v in pairs(tbl) do
+        local t = type(v)
+        if t == "string" then
+            if v:find("/") and k ~= "texture_bundle_folder" and 
+                not v:find("http") and not v:begins("mods/") and not v:find("mod_overrides") and --Beautiful
+                not v:find("Maps") and not v:find("beardlib") and not v:begins("levels/instances/mods/") and
+                not v:find(":") and not v:find('{') and not v:find('%.') and not v:find("units/mods") and not v:find("level/mods") and
+                not v:find("guis/dlcs/mods") then
+                local f = v:gsub(" '", ""):gsub("' ", ""):gsub("'", "")
+                LogFound(f)
+            end
+        elseif t == "table" and not did[v] then
+            did[v] = true
+            tweakdata_sniffer(v)
+        end
+    end
+end
+
 log("Beginning in 5 seconds..")
 DelayedCalls:Add("アルミル・ハシュリスト", 5, function()
-    os.execute('"'..Application:base_path()..'mods\\extract_paths\\HashlistHelper.exe" banksinfo '..extract:gsub("/", "\\")..'existing_banks.banksinfo Paths.txt')
+    local file_write = "Paths.txt"
+    local search_files = false
+    os.execute('"'..Application:base_path()..'mods\\extract_paths\\HashlistHelper.exe" banksinfo '..extract:gsub("/", "\\")..'existing_banks.banksinfo '..file_write)
 
+    file = io.open(file_write, "a+")
     for id, level in pairs(tweak_data.levels) do
         if not level.custom then
             local name = level.world_name
@@ -279,19 +349,25 @@ DelayedCalls:Add("アルミル・ハシュリスト", 5, function()
                 LogFound(name.."/world_cameras")
                 LogFound(name.."/world_sounds")
 
+
                 local possible_blacklist = name.."/blacklist"
                 if DB:has("blacklist", possible_blacklist) then
                     LogFound(possible_blacklist)
                 end
-    
+
 
                 local node = ParseXml(extract..name.."/mission.mission", true, extract..BLEP.swap_endianness(string.key(name.."/mission"))..".mission")
                 if node then
+                    --Uncomment when new heists are released. It should get most of the cube lights.
+                    --[[for i=100000,100000*table.size(node) do
+                        LogFound(name.."/cube_lights/"..i, "texture", true)
+                    end]] 
+
                     for _, mission in pairs(node) do
                         local continent_path = name.."/"..mission.file
                         LogFound(continent_path)
                         local node = ParseXml(extract..continent_path..".continent", true, extract..BLEP.swap_endianness(string.key(continent_path))..".continent", true)
-                
+     
                         if node.statics then
                             for _, static in ipairs(node.statics) do
                                 local ud = static.unit_data
@@ -301,7 +377,7 @@ DelayedCalls:Add("アルミル・ハシュリスト", 5, function()
                                     end
                                 end
                                 if ud.lights or ud.projection_lights then
-                                    LogFound(name.."/cube_lights/"..ud.unit_id, "texture")
+                               --     LogFound(name.."/cube_lights/"..ud.unit_id)
                                 end
                             end
                         end
@@ -315,6 +391,7 @@ DelayedCalls:Add("アルミル・ハシュリスト", 5, function()
             end
         end
     end
+
     for id, narr in pairs(tweak_data.narrative.jobs) do
         if not narr.custom then
             if narr.load_screen then
@@ -367,11 +444,11 @@ DelayedCalls:Add("アルミル・ハシュリスト", 5, function()
         TryTexture("guis/textures/pd2/blackmarket/icons/melee_weapons/"..id, tbl.texture_bundle_folder and "guis/dlcs/"..tbl.texture_bundle_folder.."/textures/pd2/blackmarket/icons/melee_weapons/"..id)
         TryTexture("guis/textures/pd2/blackmarket/icons/melee_weapons/outline/"..id, tbl.texture_bundle_folder and "guis/dlcs/"..tbl.texture_bundle_folder.."/textures/pd2/blackmarket/icons/melee_weapons/outline/"..id)
     end
-    
+
     for id, tbl in pairs(tweak_data.equipments) do
         TryTexture("guis/textures/pd2/blackmarket/icons/deployables/outline/"..id)
     end
-    
+
     for id, tbl in pairs(tweak_data.hud_icons) do
         LogFound(tbl.texture)
     end
@@ -382,12 +459,12 @@ DelayedCalls:Add("アルミル・ハシュリスト", 5, function()
 
     local function GetIcon(bundle_folder, cat, id)
         local guis_catalog = "guis/"
-            
+
         if bundle_folder then
             guis_catalog = guis_catalog .. "dlcs/" .. tostring(bundle_folder) .. "/"
         end
-        
-        LogFound(guis_catalog .. "textures/pd2/blackmarket/icons/"..cat.."/" .. id)
+
+        LogFound(guis_catalog .. "textures/pd2/blackmarket/icons/"..cat.."/" .. id, "texture")
     end
 
     for id, tbl in pairs(tweak_data.blackmarket.materials) do
@@ -403,16 +480,61 @@ DelayedCalls:Add("アルミル・ハシュリスト", 5, function()
         GetIcon(tbl.texture_bundle_folder, "masks", id)
     end
 
+    local function search_tbl_stuff(tbl)
+        if tbl.unit then
+            LogFound(tbl.unit)
+        end
+        if tbl.material then
+            LogFound(tbl.material)
+        end
+        if tbl.third_unit then
+            LogFound(tbl.third_unit)
+        end
+        if tbl.texture then
+            LogFound(tbl.third_unit)
+        end
+    end
+
+    for id, tbl in pairs(tweak_data.blackmarket.player_styles) do
+        GetIcon(tbl.texture_bundle_folder, "player_styles", id)
+        search_tbl_stuff(tbl)
+        if tbl.characters then
+            for _, character in pairs(tbl.characters) do
+                search_tbl_stuff(character)
+                if character.material_variations then
+                    LogFound(character.material_variations.third_material)
+                end
+            end
+        end
+    end
+
+    for id, char in pairs(tweak_data.blackmarket.characters) do
+        GetIcon(char.texture_bundle_folder, "character", id)
+    end
+
     for id, tbl in pairs(tweak_data.weapon.factory.parts) do
         GetIcon(tbl.texture_bundle_folder, "mods", id)
+        if tbl.unit then
+            LogFound(tbl.unit)
+            LogFound(tbl.unit.."_thq", "material_config")
+            LogFound(tbl.unit.."_cc_thq", "material_config")
+            LogFound(tbl.unit.."_thq_cc", "material_config")
+            LogFound(tbl.unit.."_thq_thq", "material_config")
+            LogFound(tbl.unit.."_thq_cc_thq", "material_config")
+            LogFound(tbl.unit.."_cc", "material_config")
+        end
 	end
-    
+
     for cat, tbl in pairs(tweak_data.economy) do
+        if cat == "rarities" then
+            LogFound(cat.header_col)
+            LogFound(cat.bg_texture)
+        end
         if cat == "safes" or cat == "drills" or cat == "armor_skins" then
             for id, td in pairs(tbl) do
                 local guis_catalog = "guis/"
                 local bundle_folder = td.texture_bundle_folder
-        
+
                 if bundle_folder then
                     guis_catalog = guis_catalog .. "dlcs/" .. tostring(bundle_folder) .. "/"
                 end
@@ -421,6 +543,9 @@ DelayedCalls:Add("アルミル・ハシュリスト", 5, function()
         end
     end
 
+    tweakdata_sniffer(tweak_data)
+
     --Comment to search only lua.
     find_all(extract)
+    file:close()
 end)
